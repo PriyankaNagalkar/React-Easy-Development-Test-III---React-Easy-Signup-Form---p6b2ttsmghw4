7ed7e3ce3b31023c02dfb0302b6c72c1956999c4
Priyanka Rajesh Nagalkar <93524881+PriyankaNagalkar@users.noreply.github.com>
2022-02-04 19:14:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
+// import '../styles/App.css';
 
+import React from 'react'
 
 const App = () => {
   
   return (
-
+    <div>
+      <form>
+        name: <input type="text" id='name'></input><br></br>
+        email: <input type="text" id="email"></input><br></br>
+        password: <input type="password" id='password'></input><br></br>
+        <input type="checkbox" id="consent"></input><br></br>
+       <button>Signup</button>
+      </form>
+    </div>
   )
 }
 
